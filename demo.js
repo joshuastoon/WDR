83b600bf4395962c6511bb75b466cc4fbe681af1
Joshua Wooton <joshuastoon209@gmail.com>
2019-07-08 15:12:02 -0600

    some stuff fuxed, still working on it

---
+++ demo.js
@@ -0,0 +1,11 @@
+
+let yourName= "Josh";
+console.log(yourName)
+
+function sayYourName (name) {
+    console.log("Hello " + yourName + ", how are you doing?");
+    console.log("Hello " + name + ", how are you doing?");
+}
+
+sayYourName("Bob");
+sayYourName("Sally");
