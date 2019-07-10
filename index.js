f213f7cc24446a370d37dcdc89a8e40c9fa64ced
Joshua Wooton <joshuastoon209@gmail.com>
2019-07-10 13:59:43 -0600

    still working on solution

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
-let randomNum = Math.floor(Math.random() * 99);
+var randNum = Math.round( Math.random() * 9 ) + 1;
+
 
 makeGuess = (form) => {
     let guessedNum = parseInt(form.inputbox.value, 10);
     form.inputbox.value = "";
-    let text = 'You guessed: ' + guessedNum ;
-    if(guessedNum === randomN){
+    let response = 'You guessed: ' + guessedNum ;
+    if(guessedNum === randDum){
         text += '<br> Your Answer was correct!';
     }
     else {
         text += '<br> You are Wrong!';
     }
-    document.getElementById("response").innerHTML = text;
+    document.getElementById("response").innerText = text;
 }
 function sendData(form) {
-         var guessNum = form.inputbox.value;
+         var guessNum = form.inputbox.value;
     }
+    
---
+++ objects.js
@@ -0,0 +1,14 @@
+let myDog = {
+    coatType: "fluffy",
+    breed: "Husky",
+    color:'White',
+    bark: function bark(){
+        console.log('woof');
+    },
+    describeYourself: function describeYourself(){
+        console.log("Hello, I am a" = this.breed)
+    }
+}
+
+console.log(myDog.coatType);
+myDog.bark();
